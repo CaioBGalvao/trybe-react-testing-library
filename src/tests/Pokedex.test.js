b68ea3da0b2acb7c6fbe3e56ba8665cb3d3c6993
CaioBGalvao <caio_bgalvao@hotmail.com>
2022-05-17 14:40:15 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -37,41 +37,41 @@ describe('Requisito 5', () => {
     });
   });
 
-  // it('Testa se a Pokédex tem os botões de filtro.', () => {
-  //   renderWithRouter(<App />);
-  //   const allBottons = [
-  //     /all/i,
-  //     /electric/i,
-  //     /fire/i,
-  //     /bug/i,
-  //     /poison/i,
-  //     /psychic/i,
-  //     /normal/i,
-  //     /dragon/i,
-  //   ];
-  //   const allTypes = [
-  //     /electric/i,
-  //     /electric/i,
-  //     /fire/i,
-  //     /bug/i,
-  //     /poison/i,
-  //     /psychic/i,
-  //     /normal/i,
-  //     /dragon/i,
-  //   ];
-  //   allBottons.forEach((type, index) => {
-  //     const allButtons = screen.getByRole('button', { name: type });
-  //     expect(allButtons).toBeInTheDocument();
-  //     userEvent.click(allButtons);
-  //     const pkmnType = screen.getByTestId('pokemon-type');
-  //     console.log(pkmnType);
-  //     expect(pkmnType).stringMatching(allTypes[index]);
-  //   });
-  // });
+  it('Testa se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: /all/i });
+    expect(buttonAll).toBeInTheDocument();
+    const filtersButtons = screen.getAllByTestId('pokemon-type-button');
+    expect(filtersButtons[0]).toBeInTheDocument();
+
+    userEvent.click(filtersButtons[2]);
+
+    expect(filtersButtons[2]).toHaveTextContent('Bug');
+    let pkmnName = screen.getByText('Caterpie');
+    let pkmnType = screen.getAllByText('Bug');
+    let pkmnWeight = screen.getByText('Average weight: 2.9 kg');
+    expect(pkmnName).toBeInTheDocument();
+    expect(pkmnType[0]).toBeInTheDocument();
+    expect(pkmnWeight).toBeInTheDocument();
+
+    userEvent.click(filtersButtons[4]);
+    expect(filtersButtons[4]).toHaveTextContent('Psychic');
+    pkmnName = screen.getByText('Alakazam');
+    pkmnType = screen.getAllByText('Psychic');
+    pkmnWeight = screen.getByText('Average weight: 48.0 kg');
+    expect(pkmnName).toBeInTheDocument();
+    expect(pkmnType[0]).toBeInTheDocument();
+    expect(pkmnWeight).toBeInTheDocument();
+  });
   it('Testa se a Pokédex contém um botão para resetar o filtro', () => {
     renderWithRouter(<App />);
     const resetButton = screen.getByRole('button', { name: /all/i });
     expect(resetButton).toBeInTheDocument();
-    expect(resetButton.name).toBe(/all/i);
+    userEvent.click(resetButton);
+
+    const fistPkmn = screen.getByText('Pikachu');
+    const secondPkmn = screen.getAllByText('Electric');
+    expect(fistPkmn).toBeInTheDocument();
+    expect(secondPkmn[0]).toBeInTheDocument();
   });
 });
